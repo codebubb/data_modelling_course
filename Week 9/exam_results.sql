1fbff0c8e2279318952f940368dcfa33c831a12e
James Bubb <jpbubb82@gmail.com>
2016-10-10 15:52:10 +0000

    Add week 9 files.

---
+++ Week 9/exam_results.sql
@@ -0,0 +1,29 @@
+CREATE TABLE exams (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, subject TEXT, mark INTEGER, grade CHAR(1));
+
+
+INSERT INTO exams (name, subject, mark, grade) VALUES
+
+('Sophie George','Maths', 66, 'B'),
+('Sophie George','English', 72, 'A'),
+('Sophie George','Chemistry', 55, 'C'),
+('Sophie George','Computer Science', 88, 'D'),
+('Dean Burke','Maths',53, 'C'),
+('Dean Burke','English', 68, 'B'),
+('Dean Burke','Chemistry', 71, 'A'),
+('Dean Burke','Computer Science', 79,'A'),
+('Alison Warren','Maths', 73, 'A'),
+('Alison Warren','English', 44, 'D'),
+('Alison Warren','Chemistry', 82, 'A'),
+('Alison Warren','Computer Science', 90,'A'),
+('Arthur Fox','Maths', 83, 'A'),
+('Arthur Fox','English', 63, 'B'),
+('Arthur Fox','Chemistry', 76, 'A'),
+('Arthur Fox','Computer Science', 56, 'C'),
+('Jo Morris','Maths', 37, 'E'),
+('Jo Morris','English', 41, 'D'),
+('Jo Morris','Chemistry', 35, 'E'),
+('Jo Morris','Computer Science', 45, 'D'),
+('Tony Meyer','Maths', 88, 'A'),
+('Tony Meyer','English', 85, 'A'),
+('Tony Meyer','Chemistry', 81, 'A'),
+('Tony Meyer','Computer Science', 83, 'A');
